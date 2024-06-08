351a1f9e58f681f7c930fb9f1c902f43d78b151a
sapnaf13 <imsapnaf13@gmail.com>
2024-06-08 12:00:07 +0530

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Harish Fulwani on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
